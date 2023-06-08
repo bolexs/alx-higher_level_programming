befd1053f94af6093bc254455c10f650fd53990d
bolexs <boluwatufeadeojo@gmail.com>
2023-06-08 15:45:39 -0700

    new files

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 if __name__ = "__main__":
     import sys
-    if(len(sys.argv) == 1):
-        print("0 arguments.")
-    elif len(sys.argv) == 2:
-        print("1 argument:")
-        print(f"1: {sys.argv[1]}")
-    elif len(sys.argv) > 2:
-        print(f"{len(sys.argv) - 1} arguments:")
-        for count in sys.argv[1:]:
-            print(f"{sys.argv.index(count)}: {count}")
+    i = len(sys.argv) - 1
+
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
+    else:
+        print("{} arguments:".format(i))
+
+    if i >= 1:
+        i = 0
+        for arg in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, arg))
+            i += 1
