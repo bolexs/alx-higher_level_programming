353b26612db22c693617e346f6103190db80fdd1
bolexs <boluwatufeadeojo@gmail.com>
2023-06-08 15:47:58 -0700

    new files

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ = "__main__":
     import sys
-    sum = 0
-    for arg in sys.argv:
-        if arg != sys.argv[0]:
-            sum += int(arg)
-    print(sum)
+    result = 0
+
+    for arg in sys.argv[1:]:
+        result += int(arg)
+    print(result)
