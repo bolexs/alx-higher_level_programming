d60ff0061880dae6efa03d7685e70123957b80cb
bolexs <boluwatufeadeojo@gmail.com>
2023-06-12 09:22:47 -0700

    new scripts

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    check_div = []
+    for num in range(len(my_list)):
+        if my_list[num] % 2 == 0:
+            check_dev.append(True)
+        else:
+            check_div.append(False)
+    return check_div
