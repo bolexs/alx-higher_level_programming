f72c30a368c527a14753c1e2bd6c3a38bc0f6875
bolexs <boluwatufeadeojo@gmail.com>
2023-06-15 00:47:56 -0700

    new scripts

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+
 def uniq_add(my_list=[]):
-    uni_list = set(my_list)
-    num = 0
-    for i in uni_list:
-        num += i
-        return num
+    count = 0
+    for i in set(my_list):
+        count += i
+    return count
