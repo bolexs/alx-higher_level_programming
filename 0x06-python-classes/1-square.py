625e1979fb2faee041c98a7099be4b7137f8cbae
bolexs <boluwatufeadeojo@gmail.com>
2023-06-27 14:59:43 -0700

    changes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Define a Square
+    """
+    def __init__(self, size):
+        """
+        Initializes a new instance of the Squae class with the params
+        """
+        self.__size = size
