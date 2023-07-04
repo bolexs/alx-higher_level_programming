d74d345945e0f3afad048f1869aa01eb06e57e6f
bolexs <boluwatufeadeojo@gmail.com>
2023-07-04 06:40:42 -0700

    changes

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,56 @@
+
+    @width.setter
+    def width(self, value):
+        """Sets/defines the width"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Return the height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets/defines the height"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Calculates and returns the area of the rectangle"""
+        a = self.__width
+        b = self.__height
+        return a * b
+
+    def perimeter(self):
+        """Calculates and returns the perimeter of the rectangle"""
+        w = self.__width
+        h = self.__height
+        peri = 2 * (w + h)
+        if w == 0 or h == 0:
+            peri = 0
+        return "{}".format(peri)
+
+    def __str__(self):
+        """ Method that returns the Rectangle #
+
+        Returns:
+            str of the rectangle
+
+        """
+
+        rectangle = ""
+
+        if self.width == 0 or self.height == 0:
+            return rectangle
+
+        for i in range(self.height):
+            rectangle += ("#" * self.width) + "\n"
+
+        return rectangle[:-1]
