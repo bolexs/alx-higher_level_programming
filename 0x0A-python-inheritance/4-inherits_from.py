b9e47053c73d9fe9ae4b7dd0737af913ce735878
bolexs <boluwatufeadeojo@gmail.com>
2023-07-10 15:26:26 -0700

    changes

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a class"""
+
+
+def inherits_from(obj, a_class):
+    """ Function that returns True/False if obj is an instance of a_class
+
+    Args:
+        obj: object
+        a_class: class type
+
+    Returns:
+        True if obj is an instance of a_class
+        False, otherwise
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
