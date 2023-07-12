eb72e3cf6fbef63737bd34ca66757a742301f6bd
bolexs <boluwatufeadeojo@gmail.com>
2023-07-11 17:20:28 -0700

    changes

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defines a Pascal's Triangle"""
+
+
+def pascal_triangle(n):
+    """Returns a list of lists of integers representing
+        the Pascal's triangle of n"""
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        prev_row = triangle[i - 1]
+
+        for j in range(1, i):
+            element = prev_row[j - 1] + prev_row[j]
+            row.append(element)
+
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
