24e3680869561c42d1c9b2e438d0251504fd1c4a
bolexs <boluwatufeadeojo@gmail.com>
2023-10-24 20:04:25 +0100

    starwars the clone wars~

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,18 +3,18 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, function (err, response, body) {
-    if (err) {
-        console.log(err);
-    } else {
-        const movies = JSON.parse(body).results;
-        let counter = 0;
-        for (const movie of movies) {
-            for (const character of movie.characters) {
-                if (character.includes('18')) {
-                    counter++;
-                }
-            }
+  if (err) {
+    console.log(err);
+  } else {
+    const movies = JSON.parse(body).results;
+    let counter = 0;
+    for (const movie of movies) {
+      for (const character of movie.characters) {
+        if (character.includes('18')) {
+          counter++;
         }
-        console.log(counter);
+      }
     }
-})
+    console.log(counter);
+  }
+});
